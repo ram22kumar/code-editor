61ef5b88fbef547c1a80015df2fb583ce194b05d
Ram Kumar <ramkumar2292000@gmail.com>
2026-02-25 07:50:59 -0500

    Added the test for mongodb

--- src/main/java/com/realtime/code_editor/CodeEditorApplication.java
+++ src/main/java/com/realtime/code_editor/CodeEditorApplication.java
@@ -1,11 +1,20 @@
 package com.realtime.code_editor;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodeEditorApplication {
 
+	@Value("${spring.data.mongodb.uri:NOT_SET}")
+	private String mongoUri;
+
+	@PostConstruct
+	public void logMongoUri() {
+		System.out.println("Mongo URI LOADED = " + mongoUri);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CodeEditorApplication.class, args);
 	}
